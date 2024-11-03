1deab6140d32a8f782446f5d205efc550e881d64
Angie Zuniga <anyopolito13@gmail.com>
2024-11-03 12:38:49 -0500

    proptech agent tutorial

--- chat.py
+++ chat.py
@@ -1,18 +1,25 @@
 import streamlit as st
-from llm_utils import call_llm_with_history_messages
+from proptech_agent import graph_builder    
+from langchain_core.messages import HumanMessage
+import uuid
 
-st.title("💬 Chatbot")
+thread_id = str(uuid.uuid4())
+st.title("💬 ChatBot")
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = []
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
-
+    
+# Medical assistant
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = call_llm_with_history_messages(prompt=prompt, messages=st.session_state.messages)
-    msg = response
+    response = graph_builder.invoke(
+        {"messages": [HumanMessage(content=prompt)]+st.session_state.messages},
+        config={"configurable": {"thread_id": thread_id}}
+    )
+    msg = response["messages"][-1].content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
